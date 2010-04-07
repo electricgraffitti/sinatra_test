f8e0559869c2e60db7f31ed6ea23b9441dcc52bb
Bob <bob@electricgraffitti.com>
2010-04-07 13:35:18 -0600

    crapola

--- config.ru
+++ config.ru
@@ -1,8 +1,17 @@
 require 'rubygems'
 require 'sinatra'
+require 'app.rb'
+path = ''
 
-require 'app'
-
+set :root, path
+set :views, path + '/views'
+set :public,  path + '/public'
+set :run, false
 set :environment, :production
+set :raise_errors, true
 
-run Sinatra.application
+log = File.new("sinatra.log", "a")
+STDOUT.reopen(log)
+STDERR.reopen(log)
+
+run Sinatra::Application
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,33 @@
 #========================
 #CONFIG
 #========================
-set :application, "sinatra_base"
+set :application, "hcirack"
+set :deploy_to, "/var/www/iphoneapps/sinatra_base"
 set :scm, :git
-set :git_enable_submodules, 1
 set :repository, "git@github.com:electricgraffitti/sinatra_test.git"
 set :branch, "master"
-set :ssh_options, { :forward_agent => true }
-set :stage, :production
 set :user, "rails"
+
+set :ssh_options, { :forward_agent => true }
 set :use_sudo, false
-set :runner, "rails"
-set :deploy_to, "/var/www/iphoneapps/#{application}"
-set :domain, "hcirack"
 #========================
 #ROLES
 #========================
-role :app, domain
-role :web, domain
-role :db, domain, :primary => true
+role :app, "#{application}"
+role :web, "#{application}"
+role :db, "#{application}", :primary => true
 #========================
 #CUSTOM
 #========================
 namespace :deploy do
-task :start, :roles => :app do
-run "touch #{current_release}/tmp/restart.txt"
-end
-task :stop, :roles => :app do
-# Do nothing.
-end
-desc "Restart Application"
-task :restart, :roles => :app do
-run "touch #{current_release}/tmp/restart.txt"
-end
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+  task :stop, :roles => :app do
+  # Do nothing.
+  end
+  desc "Restart Application"
+    task :restart, :roles => :app do
+      run "touch #{current_release}/tmp/restart.txt"
+    end
 end
