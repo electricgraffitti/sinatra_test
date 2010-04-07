5fa6e97fce9ca30d6bdead1bd802302e06d3ec42
Bob <bob@electricgraffitti.com>
2010-04-07 12:46:11 -0600

    more studf

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,19 @@
 set :application, "sinatra_base"
-set :server, "hcirack"
 set :domain, 'hciapps.com'
 set :deploy_to, "/var/www/iphoneapps/#{application}"
 set :repository, "git@github.com:electricgraffitti/sinatra_test.git"
-set :branch, "master"
+set :branch, 'master'
 set :user, "rails"
-
 set :scm, :git
-set :ssh_options, { :forward_agent => true }
-set :use_sudo, true
+set :git_enable_submodules, 1
+ssh_options[:paranoid] = false
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+set :keep_releases, 2
 
-role :app, "#{server}"
-role :web, "#{server}"
-role :db, "#{server}", :primary => true
+role :app, "#{domain}"
+role :web, "#{domain}"
+role :db, "#{domain}", :primary => true
 
 namespace :deploy do
   task :restart, :roles => :app do
